f0e49328deca25d909cff9c1bf3faf36edca52e5
wesleyxue28 <wesley_xue28@milton.edu>
2026-05-11 21:42:53 -0400

    did this for homework

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,49 @@
 public class Player {
 
-    public String name;
-    public Card[] hand;
-    public int handCount;
-    public int chips;
+    public boolean isDealer = false;
+    public int numPlayer;
+    public Card [] hand;
+    public int numCards;
+    public boolean isBust;
 
-    public Player(String pName, int pChips) {
-        name = pName;
-        chips = pChips;
-        hand = new Card[10];
-        handCount = 0;
+    public int cardValue;
+
+    public static void main(String[] args) {
+        Player player = new Player();
     }
-    
+
+
+    public Player (int pnumPlayer){
+        numPlayer = pnumPlayer;
+        hand = new Card [11];
+        numCards = 0;
+        isBust = false;
+    }
+
+    public void printPlayer(){
+        for (int i = 0; i < numCards; i++){
+            hand[i].printCard();
+        }
+    }
+
+    public void addCard(Card newCard){
+        hand [numCards] = newCard;
+        numCards++;
+    }
+
+    public void getHandTotal(){
+        int total = 0;
+        for (int i = 0; i < numCards; i++){
+            if (hand[i].value == 0) {
+                total += 11;
+            } else if (hand[i].value >= 10) {
+                total += 10;
+            } else {
+                total += hand[i].value + 1;
+            }
+            hand[i].printCard();
+        }
+    }
+
+
+    }
